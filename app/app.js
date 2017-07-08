8a03a7f439949fba8e3adac02fad1068fd4ed390
nivalen292 <nivalen292@outlook.com>
2017-07-08 15:50:42 +0300

    added path dependency

--- app/app.js
+++ app/app.js
@@ -1,5 +1,8 @@
+/* globals __dirname */
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const init = (db) => {
 
@@ -10,14 +13,14 @@ const init = (db) => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
 
-    app.use('/static', express.static('../public'));
-    app.use('/libs', express.static(('../node_modules')));
+    app.use('/static', express.static(path.join(__dirname, '../public/')));
+    app.use('/libs', express.static(path.join(__dirname, '../node_modules/')));
 
-    app.get("/", (request, response) => {
+    app.get('/', (request, response) => {
         response.render('layout');
     });
 
-    app.get("/about", (request, response) => {
+    app.get('about', (request, response) => {
         response.render('about');
     });
 
