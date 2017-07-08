78489e27043f425d38d74c41150421a690c6bb5f
Milena Shiskova <shishkova.milena@gmail.com>
2017-07-08 14:07:44 +0300

    Changes on server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const asyc = () => {
 const config = require('./app/config');
 
 async()
-    .then(() => require('./db').init(config.connectionString))
-    .then((db) => require('./data').init(db))
+.then(() => require('./db').init(config.connectionString))
+    .then((db) => require('./app').init(db))
     // .then((data) => require('./app').init(data))
     .then((app) => {
         app.listen(config.port, () => console.log(`Server runnit at localhost:${config.port}`));
