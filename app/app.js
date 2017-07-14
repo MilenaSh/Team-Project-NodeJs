dfb28fbabbff5746e085b17b56c2326119458ce4
nivalen292 <nivalen292@outlook.com>
2017-07-14 12:07:45 +0300

    bug fix

--- app/app.js
+++ app/app.js
@@ -17,46 +17,8 @@ const init = (db) => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(cookieParser());
-    // app.use(session({
-    //     secret: 'the camp alpha',
-    //     resave: true,
-    //     saveUninitiallized: true,
-    // }));
 
     const passport = require('./passport').passportSetUp(app, db);
-
-    // const AuthStrategy = new LocalStrategy((username, password, done) => {
-    //     db.collection('users')
-    //         .find({ username: username })
-    //         .toArray()
-    //         .then((user) => {
-    //             if (user.length > 0 && (user[0].password === password)) {
-    //                 done(null, user[0]);
-    //             } else {
-    //                 done(null, false);
-    //             }
-    //         })
-    //         .catch(error => done(error, false));
-    // });
-
-    // passport.use(AuthStrategy);
-
-    // passport.serializeUser((user, done) => {
-    //     if (user) {
-    //         done(null, user._id);
-    //     }
-    // });
-
-    // passport.deserializeUser((userId, done) => {
-    //     db.collection('users')
-    //         .find({ _id: ObjectId(userId) })
-    //         .toArray()
-    //         .then(user => done(null, user || false))
-    //         .catch(error => done(error, false));
-    // });
-
-    // app.use(passport.initialize());
-    // app.use(passport.session());
 
     app.use('/static', express.static(path.join(__dirname, '../public/')));
     app.use('/libs', express.static(path.join(__dirname, '../node_modules/')));
