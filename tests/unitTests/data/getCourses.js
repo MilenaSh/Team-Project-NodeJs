e203e26e8a2c817b839b2fd01e639f05c105a18c
TihomirTE <tihomirteofilov@gmail.com>
2017-07-21 17:41:13 +0300

    Add some semi-correct tests for the Data

--- tests/unitTests/data/getCourses.js
+++ tests/unitTests/data/getCourses.js
@@ -26,6 +26,7 @@ describe('Get data for the courses', () => {
             .callsFake(() => {
                 return { find };
             });
+
         data = init(db);
     });
 
@@ -36,8 +37,107 @@ describe('Get data for the courses', () => {
     // Has to use data
     it('expect to return courses', () => {
         return ((collection) => {
-            console.log();
             expect(collection).to.equal(courses);
+        });
+    });
+
+    it('Get courses', () => {
+        data = init(db);
+        data.then(function(d) {
+            return d.getCourses()
+                .then((coursesCollection) => {
+                    expect(coursesCollection).to.be.equal(courses);
+                });
+        });
+    });
+
+    it('Get course by Id', () => {
+        const id = 1;
+        data = init(db);
+        data.then(function(d) {
+            return d.getCourseById(id)
+                .then((coursesCollection) => {
+                    expect(coursesCollection).to.be.equal(courses);
+                });
+        });
+    });
+
+    it('Push liked user', () => {
+        const title = 'NodeJS';
+        const lecturer = 'Doncho';
+        const user = 'Pesho';
+        const update = () => {
+            return {
+                title,
+                lecturer,
+                user,
+            };
+        };
+
+        sinon.stub(db, 'course')
+            .callsFake(() => {
+                return { update };
+            });
+
+        data.then(function(d) {
+            return d.pushLikedUser(title, lecturer, user)
+                .then((col) => {
+                    expect(col).to.be.equal(courses);
+                })
+                .catch(function() {
+                    console.log('Error');
+                });
+        });
+    });
+
+    it('Pull liked user', () => {
+        const title = 'NodeJS';
+        const lecturer = 'Doncho';
+        const user = 'Pesho';
+        const update = () => {
+            return {
+                title,
+                lecturer,
+                user,
+            };
+        };
+
+        sinon.stub(db, 'coursesCollection')
+            .callsFake(() => {
+                return { update };
+            });
+
+        // const coursesCollection = ['JS'];
+        // update = coursesCollection.update.bind(coursesCollection);
+
+        data.then(function(d) {
+            return d.pullLikedUser(title, lecturer, user)
+                .then((col) => {
+                    expect(col).to.be.equal(courses);
+                });
+        });
+    });
+
+    it('Push enrolled courses', () => {
+        const course = 'JS';
+        const courseId = 1;
+        const userId = 2;
+        const findOne = () => {
+            return {
+                courseId,
+            };
+        };
+
+        sinon.stub(db, 'coursesCollection')
+            .callsFake(() => {
+                return { findOne };
+            });
+        data = init(db);
+        data.then(function(d) {
+            return d.pushEnrolledCourse(courseId, userId)
+                .then((foundCourse) => {
+                    expect(foundCourse).to.be.equal(course);
+                });
         });
     });
 });
