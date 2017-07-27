c2e6362172dbcdfe750dc052a743ad5b4da4e739
Milena Shiskova <shishkova.milena@gmail.com>
2017-07-27 20:26:06 +0300

    Added registration validations

--- app/passport/userValidator.js
+++ app/passport/userValidator.js
@@ -1,25 +1,32 @@
+const userValidator = (() => {
     const usernameMinLength = 4;
     const usernameContent = new RegExp('^[a-zA-Z0-9]');
-    const passwordMinLength = 8;
-    const errorMessage = '';
+    //  const passwordMinLength = 8;
+    let errorMessage = '';
 
-    const userValidator = {
+    function getErrorMessage() {
+        return errorMessage;
+    }
 
-        getErrorMessage() {
-            return errorMessage;
-        },
+    function validateUsername(username) {
+        if (username.length === 0) {
+            errorMessage = 'Username cannot be empty!';
+            throw new Error(errorMessage);
+        } else if (username.length < usernameMinLength) {
+            errorMessage = 'Username must be at least 4 symbols!';
+            throw new Error(errorMessage);
+        }
+        return errorMessage;
+    }
 
-        validateUsername(username) {
-            if (username.length === 0) {
-                errorMessage = 'Username cannot be empty!';
-                throw new Error('Username cannot be empty!');
-            }
-        },
+    function validateUser(user) {
+        validateUsername(user.username);
+    }
 
-        validateUser(user, cb) {
-            this.validateUsername(user.username);
-            cb('error', errorMessage);
-        },
+    return {
+        validateUser: validateUser,
+        getErrorMessage: getErrorMessage,
     };
+})();
 
-    module.exports = { userValidator };
+module.exports = { userValidator };
